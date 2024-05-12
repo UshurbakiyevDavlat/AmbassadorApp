aea0bb88bfadf8be9d7221209bc31beb18313f0f
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-05-12 20:18:37 +0500

    added job for order completed event, updated consumer to unserialize sent job and handle it

--- email-microservice/app/Queues/KafkaQueue.php
+++ email-microservice/app/Queues/KafkaQueue.php
@@ -44,7 +44,8 @@ class KafkaQueue extends Queue implements \Illuminate\Contracts\Queue\Queue
 
         switch ($message->err) {
             case RD_KAFKA_RESP_ERR_NO_ERROR:
-                var_dump($message->payload);
+                $job = unserialize($message->payload);
+                $job->handle();
                 break;
             case RD_KAFKA_RESP_ERR__PARTITION_EOF:
                 echo "No more messages; will wait for more\n";
