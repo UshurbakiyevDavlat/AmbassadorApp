3e9be3a028f7e742c6185d2003a9c95668906f2f
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-05-17 12:55:12 +0500

    updated kafka package namespace

--- ambassador-monolith/config/app.php
+++ ambassador-monolith/config/app.php
@@ -174,7 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        \Davlatbekushurbakiyev\KafkaQueue\KafkaServiceProvider::class,
+        Kafka\KafkaServiceProvider::class,
     ],
 
     /*
