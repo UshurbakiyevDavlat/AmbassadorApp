4be16702af044c47fbee55a0dbfaf00f7563072a
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-06-16 15:42:03 +0500

    refactored useles parts

--- ambassador-monolith/app/Http/Controllers/LinkController.php
+++ ambassador-monolith/app/Http/Controllers/LinkController.php
@@ -15,11 +15,6 @@ use Illuminate\Support\Str;
 
 class LinkController extends Controller
 {
-    public function __construct(private readonly UserService $service)
-    {
-
-    }
-
     /**
      * Index links method
      *
@@ -31,41 +26,5 @@ class LinkController extends Controller
         $links = Link::with('orders')->where('user_id', $id)->get();
 
         return LinkResource::collection($links);
-    }
-
-    /**
-     * Store links method
-     *
-     * @param Request $request
-     * @return Model|Link
-     * @throws Exception
-     */
-    public function store(Request $request): Model|Link
-    {
-        $user = $this->service->profile();
-
-        $link = Link::create([
-            'user_id' => $user['id'],
-            'code' => Str::random(6)
-        ]);
-
-        $linkProducts = [];
-
-        foreach ($request->input('products') as $product_id) {
-            $linkProduct = LinkProduct::create([
-                'link_id' => $link->id,
-                'product_id' => $product_id
-            ]);
-
-            $linkProducts[] = $linkProduct->toArray();
-        }
-
-        LinkCreatedJob::dispatch([
-            'link' => $link->toArray(),
-            'linkProducts' => $linkProducts,
-        ])
-            ->onQueue('checkout_topic');
-
-        return $link;
     }
 }
--- ambassador-monolith/app/Http/Controllers/ProductController.php
+++ ambassador-monolith/app/Http/Controllers/ProductController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\ProductUpdatedEvent;
 use App\Jobs\ProductCreatedJob;
 use App\Jobs\ProductDeletedJob;
 use App\Jobs\ProductUpdatedJob;
@@ -10,9 +9,6 @@ use App\Models\Product;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Str;
-use Psr\SimpleCache\InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -82,68 +78,5 @@ class ProductController extends Controller
         ProductDeletedJob::dispatch(['id' => $product['id']])->onQueue('checkout_topic');
 
         return response(null, Response::HTTP_NO_CONTENT);
-    }
-
-    /**
-     * Get products for frontend method
-     *
-     * @return mixed
-     * @throws InvalidArgumentException
-     */
-    public function frontend(): mixed
-    {
-        if ($products = \Cache::get('products_frontend')) {
-            return $products;
-        }
-
-        $products = Product::all();
-
-        \Cache::set('products_frontend', $products, 30 * 60); //30 min
-
-        return $products;
-    }
-
-    /**
-     * Get products for backend method
-     *
-     * @param Request $request
-     * @return array
-     */
-    public function backend(Request $request): array
-    {
-        $page = $request->input('page', 1);
-
-        /** @var Collection $products */
-        $products = \Cache::remember('products_backend', 30 * 60, fn() => Product::all());
-
-        if ($s = $request->input('s')) {
-            $products = $products
-                ->filter(
-                    fn(Product $product) => Str::contains($product->title, $s) || Str::contains($product->description, $s)
-                );
-        }
-
-        $total = $products->count();
-
-        if ($sort = $request->input('sort')) {
-            if ($sort === 'asc') {
-                $products = $products->sortBy([
-                    fn($a, $b) => $a['price'] <=> $b['price']
-                ]);
-            } else if ($sort === 'desc') {
-                $products = $products->sortBy([
-                    fn($a, $b) => $b['price'] <=> $a['price']
-                ]);
-            }
-        }
-
-        return [
-            'data' => $products->forPage($page, 9)->values(),
-            'meta' => [
-                'total' => $total,
-                'page' => $page,
-                'last_page' => ceil($total / 9)
-            ]
-        ];
     }
 }
