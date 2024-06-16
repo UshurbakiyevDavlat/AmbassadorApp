a0baa3e7d2e31f20e11122ebd7a302dd1e8a18cd
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-06-16 16:13:29 +0500

    added kafka config

--- ambassador-microservice/app/Jobs/OrderCompletedJob.php
+++ ambassador-microservice/app/Jobs/OrderCompletedJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Order;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -28,5 +29,11 @@ class OrderCompletedJob implements ShouldQueue
      */
     public function handle(): void
     {
+        Order::create([
+            'id' => $this->data['id'],
+            'user_id' => $this->data['user_id'],
+            'code' => $this->data['code'],
+            'total' => $this->data['ambassador_revenue'],
+        ]);
     }
 }
