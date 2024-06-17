0ff952863c098324a911c638c1d450fcb25d486b
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-06-17 12:54:02 +0500

    added cors and registered middleware as an alias

--- admin-microservice/bootstrap/app.php
+++ admin-microservice/bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->use(['scope.admin' => ScopeAdminMiddleware::class]);
+        $middleware->alias([
+            'scope.admin' => ScopeAdminMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
