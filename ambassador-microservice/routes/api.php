bd3f483112c6ffaded8a4caa36bfde9908b40682
Ушурбакиев Давлатбек Алимжанович <davlatbek.ushurbakiyev@ffin.kz>
2024-06-17 14:39:29 +0500

    added redis configs and updated env example

--- ambassador-microservice/routes/api.php
+++ ambassador-microservice/routes/api.php
@@ -6,20 +6,18 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StatsController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('ambassador')->group(function () {
-    Route::post('register', [AuthController::class, 'register']);
-    Route::post('login', [AuthController::class, 'login']);
-    Route::get('products/frontend', [ProductController::class, 'frontend']);
-    Route::get('products/backend', [ProductController::class, 'backend']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('products/frontend', [ProductController::class, 'frontend']);
+Route::get('products/backend', [ProductController::class, 'backend']);
 
-    Route::middleware('scope.ambassador')->group(function () {
-        Route::get('user', [AuthController::class, 'user']);
-        Route::post('logout', [AuthController::class, 'logout']);
-        Route::put('users/info', [AuthController::class, 'updateInfo']);
-        Route::put('users/password', [AuthController::class, 'updatePassword']);
+Route::middleware('scope.ambassador')->group(function () {
+    Route::get('user', [AuthController::class, 'user']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::put('users/info', [AuthController::class, 'updateInfo']);
+    Route::put('users/password', [AuthController::class, 'updatePassword']);
 
-        Route::post('links', [LinkController::class, 'store']);
-        Route::get('stats', [StatsController::class, 'index']);
-        Route::get('rankings', [StatsController::class, 'rankings']);
-    });
+    Route::post('links', [LinkController::class, 'store']);
+    Route::get('stats', [StatsController::class, 'index']);
+    Route::get('rankings', [StatsController::class, 'rankings']);
 });
